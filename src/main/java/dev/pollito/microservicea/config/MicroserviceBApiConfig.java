4722d02988fcf0fd6d10e045f0077b88b5d03017
Fran Bec <franbecvort@gmail.com>
2024-04-01 21:13:36 +0100

    tracing working

--- src/main/java/dev/pollito/microservicea/config/MicroserviceBApiConfig.java
+++ src/main/java/dev/pollito/microservicea/config/MicroserviceBApiConfig.java
@@ -8,9 +8,11 @@ import feign.Logger;
 import feign.gson.GsonDecoder;
 import feign.gson.GsonEncoder;
 import feign.micrometer.MicrometerCapability;
+import feign.micrometer.MicrometerObservationCapability;
 import feign.okhttp.OkHttpClient;
 import feign.slf4j.Slf4jLogger;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.observation.ObservationRegistry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -28,7 +30,9 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class MicroserviceBApiConfig {
   private final MicroserviceBProperties microserviceBProperties;
-    private final MeterRegistry meterRegistry;
+  private final MeterRegistry meterRegistry;
+  private final ObservationRegistry observationRegistry;
+
   @Bean
   public HelloWorldApi microServiceBApi() {
     return Feign.builder()
@@ -38,7 +42,8 @@ public class MicroserviceBApiConfig {
         .errorDecoder(new MicroserviceBErrorDecoder())
         .logger(new Slf4jLogger(HelloWorldApi.class))
         .logLevel(Logger.Level.FULL)
-            .addCapability(new MicrometerCapability(meterRegistry))
+        .addCapability(new MicrometerObservationCapability(observationRegistry))
+        .addCapability(new MicrometerCapability(meterRegistry))
         .target(HelloWorldApi.class, microserviceBProperties.getBaseUrl());
   }
 }
